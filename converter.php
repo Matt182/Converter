dbda51f0adae0c30bed8af9d8ea881529a9efe83
matt182 <mattspring0@gmail.com>
2016-05-20 19:02:00 -0700

    phpcs test

--- converter.php
+++ converter.php
@@ -9,41 +9,37 @@ use LSS\Array2XML;
 
 function converter($in, $out)
 {
-  $extension = explode('.', $in)[1];
-  switch ($extension) {
+    $extension = explode('.', $in)[1];
+    switch ($extension) {
     case 'xml':
-      $parsed = XML2Array::createArray(file_get_contents($in));
-      break;
+            $parsed = XML2Array::createArray(file_get_contents($in));
+        break;
     case 'json':
-      $parsed = json_decode(file_get_contents($in), true);
-      break;
+            $parsed = json_decode(file_get_contents($in), true);
+        break;
     case 'yml':
-      $yaml = new Parser();
-      $parsed = $yaml->parse(file_get_contents($in));
-      break;
+            $yaml = new Parser();
+            $parsed = $yaml->parse(file_get_contents($in));
+        break;
     default:
-      # code...
-      break;
-  }
+        break;
+    }
 
-  $extension = explode('.', $out)[1];
-  switch ($extension) {
+    $extension = explode('.', $out)[1];
+    switch ($extension) {
     case 'xml':
-      Array2XML::createXML('root', $parsed)->save($out);
-      break;
+            Array2XML::createXML('root', $parsed)->save($out);
+        break;
     case 'json':
-      file_put_contents($out, json_encode($parsed));
-      break;
+            file_put_contents($out, json_encode($parsed));
+        break;
     case 'yml':
-      $dumper = new Dumper();
-      $yaml = $dumper->dump($parsed);
-      file_put_contents($out, $yaml);
-      break;
+            $dumper = new Dumper();
+            $yaml = $dumper->dump($parsed);
+            file_put_contents($out, $yaml);
+        break;
     default:
-      # code...
-      break;
-  }
+        break;
+    }
 }
-
-
 converter($argv[1], $argv[2]);
