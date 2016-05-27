8bcafe7b1469046b2e95c6a49b728c5f91af7fd7
matt182 <mattspring0@gmail.com>
2016-05-27 11:55:40 -0700

    coverage test

--- tests/fullTest.php
+++ tests/fullTest.php
@@ -6,14 +6,16 @@ class FullTest extends \PHPUnit_Framework_TestCase
     public function testJsonToYaml()
     {
         \Converter\converter(__DIR__ . 'tests\prepared.json', __DIR__ . 'tests\tested.yml');
-        $this->assertEquals(file_get_contents(__DIR__ . 'tests\prepared.yml'), file_get_contents(__DIR__ . 'tests\tested.yml'));
+        $this->assertEquals(file_get_contents(__DIR__ . 'tests\prepared.yml'),
+                            file_get_contents(__DIR__ . 'tests\tested.yml'));
         unlink(__DIR__ . 'tests\tested.yml');
     }
 
     public function testYamlToJson()
     {
         \Converter\converter(__DIR__ . 'tests\prepared.yml', __DIR__ . 'tests\tested.json');
-        $this->assertEquals(file_get_contents(__DIR__ . 'tests\prepared.json'), file_get_contents(__DIR__ . 'tests\tested.json'));
+        $this->assertEquals(file_get_contents(__DIR__ . 'tests\prepared.json'),
+                            file_get_contents(__DIR__ . 'tests\tested.json'));
         unlink(__DIR__ . 'tests\tested.json');
     }
 }
