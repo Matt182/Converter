88f3c7079974c6afd935b79b2c94ddc55fe2efa9
matt182 <mattspring0@gmail.com>
2016-05-27 12:06:31 -0700

    coverage test

--- tests/fullTest.php
+++ tests/fullTest.php
@@ -3,19 +3,24 @@ namespace Converter\tests;
 
 class FullTest extends \PHPUnit_Framework_TestCase
 {
+    private $prepJson = __DIR__ . '\prepared.json';
+    private $prepYaml = __DIR__ . '\prepared.yml';
+
+    private $testJson = __DIR__ . '\tested.json';
+    private $testYaml = __DIR__ . '\tested.yml';
+
+
     public function testJsonToYaml()
     {
-        \Converter\converter(__DIR__ . 'tests\prepared.json', __DIR__ . 'tests\tested.yml');
-        $this->assertEquals(file_get_contents(__DIR__ . 'tests\prepared.yml'),
-                            file_get_contents(__DIR__ . 'tests\tested.yml'));
-        unlink(__DIR__ . 'tests\tested.yml');
+        \Converter\converter($this->prepJson, $this->testYaml);
+        $this->assertEquals(file_get_contents($this->prepYaml),file_get_contents($this->testYaml));
+        unlink($this->testYaml);
     }
 
     public function testYamlToJson()
     {
-        \Converter\converter(__DIR__ . 'tests\prepared.yml', __DIR__ . 'tests\tested.json');
-        $this->assertEquals(file_get_contents(__DIR__ . 'tests\prepared.json'),
-                            file_get_contents(__DIR__ . 'tests\tested.json'));
-        unlink(__DIR__ . 'tests\tested.json');
+        \Converter\converter($this->prepYaml, $this->testJson);
+        $this->assertEquals(file_get_contents($this->prepJson),file_get_contents($this->testJson));
+        unlink($this->testJson);
     }
 }
